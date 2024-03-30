52ee17759e3fc410185297152c9ab8e9f8f43df8
eddygulled <edgargulay69@gmail.com>
2024-03-31 01:43:45 +0300

    Update commands list and model file name

--- App/main.py
+++ App/main.py
@@ -5,19 +5,20 @@ from tensorflow.keras import models
 from recording_helper import record_audio, terminate
 from tf_helper import preprocess_audiobuffer
 
-# !! Modify this in the correct order
-commands = ['left', 'down', 'stop', 'up', 'right', 'no', 'go', 'yes']
+#  Modify this in the correct order
+commands = ['down' 'go' 'left' 'no' 'right' 'stop' 'up' 'yes']
 
-loaded_model = models.load_model("saved_model")
+loaded_model = models.load_model("saved_model.h5")
 
 def predict_mic():
     audio = record_audio()
     spec = preprocess_audiobuffer(audio)
     prediction = loaded_model(spec)
     label_pred = np.argmax(prediction, axis=1)
-    command = commands[label_pred[0]]
-    print("Predicted label:", command)
-    return command
+    print(label_pred)
+    # command = commands[label_pred[0]]
+    # print("Predicted label:", command)
+    # return command
 
 if __name__ == "__main__":
     from turtle_helper import move_turtle
