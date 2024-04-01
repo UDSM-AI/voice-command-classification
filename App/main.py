d64f56bef1718cc56de11f5509b0464d0d7eb22d
eddygulled <edgargulay69@gmail.com>
2024-04-02 01:51:15 +0300

    Refactor audio prediction code and remove unused fingerprint module

--- App/main.py
+++ App/main.py
@@ -4,27 +4,25 @@ from tensorflow.keras import models
 
 from recording_helper import record_audio, terminate
 from tf_helper import preprocess_audiobuffer
+from fingerprint import ExportModel, label_names, load_model
 
 #  Modify this in the correct order
-commands = ['down', 'go', 'left', 'no', 'right', 'stop', 'up', 'yes']
-
-loaded_model = models.load_model("saved_model.h5")
+commands = label_names
+loaded_model  = ExportModel(load_model())
 
 def predict_mic():
     audio = record_audio()
     spec = preprocess_audiobuffer(audio)
-    prediction = loaded_model(spec)
-    label_pred = np.argmax(prediction, axis=1)
-    print(label_pred)
-    command = commands[label_pred[0]]
-    print("Predicted label:", command)
-    return command
+    results = loaded_model(spec)
+
+    predicted_class = results['class_names'].numpy()[0].decode('utf-8')
+    print(predicted_class)
 
 if __name__ == "__main__":
-    from turtle_helper import move_turtle
+    # from turtle_helper import move_turtle
     while True:
         command = predict_mic()
-        move_turtle(command)
-        if command == "stop":
-            terminate()
-            break
+        # move_turtle(command)
+        # if command == "stop":
+        #     terminate()
+        #     break
